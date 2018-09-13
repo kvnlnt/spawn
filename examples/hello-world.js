c82ade69b9067f83c82d512d68acee1bc259a09a
Kevin Lint <kevin.lint@emergencyreporting.com>
2018-09-13 08:24:33 -0700

    adds theme color

--- examples/hello-world.js
+++ examples/hello-world.js
@@ -2,12 +2,24 @@
 
 const cli = require("../bin/cli");
 
+// theme
+cli
+    .header("\n--- HELLO WORLD ---")
+    .themeColor("green");
+
 // Hello
 cli
     .command("hello", "Prints hello")
     .argument("output", "o", "Text to output")
     .callback(resp => console.log('Hello,', resp.output))
     .example("hello -o=World!", "Prints Hello World!");
+
+// Goodbye
+cli
+    .command("goodbye", "Prints hello")
+    .argument("output", "o", "Text to output")
+    .callback(resp => console.log('Goodbye,', resp.output))
+    .example("goodbye -o=World!", "Prints Goodbye World!");
 
 // Use the automated help
 cli
